efc7126cd95b797b57101034f8680c14d1dd2dd3
dodosebn <orjidominion32@gmail.com>
2024-03-06 02:26:39 +0400

    leveled

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ const saveContainer = document.querySelector(".saveDope");
 const saveAFile = document.getElementById("saveBtn");
 const SaveTOLocalStorage = document.getElementById("SaveTOLocalStorage");
 const ShowCase = document.getElementById("ShowCase");
-const Minimize = document.querySelector(".fa-window-minimize");
-Minimize.addEventListener("click", function() {
-  ReportBody.innerHeight = 0;
-});
+let greet = document.getElementById("greet");
+const today = new Date();
+const hourNow = today.getHours();
+let greeting = "";
+
+if (hourNow < 12) {
+  greeting = "Good Morning";
+} else if (hourNow < 18) {
+  greeting = "Good Afternoon";
+} else {
+  greeting = "Good Evening";
+}
+
+greet.innerHTML = greeting;
 
 if (window.innerWidth <= 380) {
   textarea.cols = 40;
@@ -21,53 +31,50 @@ if (window.innerWidth <= 380) {
 }
 
 const toggleFileDrops = () => {
-  FileDrops.style.display =
-    FileDrops.style.display === "none" ? "block" : "none";
+  FileDrops.style.display = FileDrops.style.display === "none" ? "block" : "none";
 };
 
 FileBtn.addEventListener("click", toggleFileDrops);
-FileBtn.addEventListener("dblclick", toggleFileDrops);
 FileComot.addEventListener("click", toggleFileDrops);
-
-let INPUT = textarea.value;
-console.log(INPUT);
 
 newBtn.addEventListener("click", () => {
   textarea.value = "";
   FileDrops.style.display = "none";
 });
+
 saveClick.addEventListener("click", () => {
   saveContainer.style.display = "block";
   FileDrops.style.display = "none";
 });
+
 saveClick.addEventListener("dblclick", () => {
   saveContainer.style.display = "none";
 });
 
 saveAFile.addEventListener("click", () => {
   const inputToTitle = saveInput.value;
-  Title.textContent = `${inputToTitle}`;
+  Title.textContent = inputToTitle;
   saveContainer.style.display = "none";
 });
 
 const saveTitle = () => {
-  var head = document.getElementById("head").value;
+  const head = document.getElementById("head").value;
   localStorage.setItem("head", head);
 };
 
 const saveTextarea = () => {
-  var textareaValue = document.getElementById("main-stuff").value;
+  const textareaValue = document.getElementById("main-stuff").value;
   localStorage.setItem("textarea", textareaValue);
   FileDrops.style.display = "none";
 };
 
-const LoadTitle = () => {
-  var head = localStorage.getItem("head");
+const loadTitle = () => {
+  const head = localStorage.getItem("head");
   document.getElementById("title").value = head;
 };
 
-const LoadSavedTextArea = () => {
-  var textareaValue = localStorage.getItem("textarea");
+const loadSavedTextArea = () => {
+  const textareaValue = localStorage.getItem("textarea");
   document.getElementById("main-stuff").value = textareaValue;
   FileDrops.style.display = "none";
 };
@@ -81,19 +88,19 @@ SaveTOLocalStorage.addEventListener("click", (event) => {
 
 ShowCase.addEventListener("click", (event) => {
   event.preventDefault();
-  LoadSavedTextArea();
-  saveTitle();
+  loadSavedTextArea();
+  loadTitle();
 });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", (event) => {
   if (event.ctrlKey && event.key === "s") {
     event.preventDefault();
     saveTextarea();
   }
 });
 
-document.addEventListener("keydown", function(event) {
-  if (event.ctrlKey && event.key === "O") {
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.key === "o") {
     event.preventDefault();
     fresher();
   }
@@ -104,9 +111,9 @@ function fresher() {
   FileDrops.style.display = "none";
 }
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", (event) => {
   if (event.ctrlKey && event.shiftKey && event.key === "S") {
-    event.preventDefault(); // Prevent the default browser save behavior
+    event.preventDefault();
     saveAs();
   }
 });
